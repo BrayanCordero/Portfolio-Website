7164d765fd84a32d31adcccd016527930bef9b9b
BrayanCordero <corderobrayan@yahoo.com>
2024-08-11 16:45:35 -0700

    added details to about page

--- portfolio-website/src/App.js
+++ portfolio-website/src/App.js
@@ -2,9 +2,14 @@ import React from 'react';
 import {Outlet} from 'react-router-dom'
 import Header from './components/Header';
 import Footer from './components/Footer';
+import profileImage from './assets/brayan.png'
+
+
+
 
 const App = () => (
     <div>
+      <img src={profileImage} alt='Brayan' width={'100'} height={'100'} ></img>
       <Header />
       <main><Outlet /></main>
       <Footer />
--- portfolio-website/src/components/About.js
+++ portfolio-website/src/components/About.js
@@ -2,8 +2,25 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div>
-            <h1>About Me</h1>
+        <div className='about-container'>
+            <div className='about'>
+                <h2>About Me</h2>
+                <p>As a Full Stack Developer, I specialize in crafting responsive and dynamic user interfaces with React, Redux, and JavaScript.
+                    On the back end, I bring experience in Node.js, Python, .NET, and C#, building robust and scalable applications. 
+                    With a strong foundation in Agile methodologies and a passion for problem-solving, I thrive in collaborative environments. 
+                    I am constantly seeking to refine my skills and contribute to impactful projects.
+                </p>
+            </div>
+            <div className='details'>
+                <h2>Details</h2>
+                <p>
+                    <h4>Name:</h4>
+                    <p>Brayan Cordero</p>
+                    <h4>Location:</h4>
+                    <p>San Bernardino, CA</p>
+                    <a href="https://github.com/BrayanCordero?tab=repositories" target="_blank" rel="noopener noreferrer">GitHub</a>
+                </p>
+            </div>
         </div>
     )
 }
