ca5422237c27ac1b7cb1e6fbdc3e68baf60ae097
BrayanCordero <corderobrayan@yahoo.com>
2024-08-12 17:53:24 -0700

    added css to nav bar

--- portfolio-website/src/components/Header.js
+++ portfolio-website/src/components/Header.js
@@ -1,13 +1,14 @@
 import React from "react"
 import { Link } from 'react-router-dom'
 import profileImage from '../assets/brayan.png'
+import '../styles/HeaderStyle.css'
 
 const Header = () => {
     return(
         <header>
             <nav>
                 <img src={profileImage} alt='Brayan' width={'100'} height={'100'} ></img>
-                <ul>
+                <ul className="nav">
                     <li><Link to="/">About</Link></li>
                     <li><Link to="/projects">Projects</Link></li>
                     <li><Link to="/contact">Contact</Link></li>
