2dc928a717d6da9a97db58a8647b3cd015e1d8a2
BrayanCordero <corderobrayan@yahoo.com>
2024-08-17 16:40:07 -0700

    updated about style

--- portfolio-website/src/components/About.js
+++ portfolio-website/src/components/About.js
@@ -41,22 +41,30 @@ const About = () => {
             </div>
             <div className='certifications'>
                 <h1>Certifications</h1>
-                <ul>
-                    <a href="https://www.codecademy.com/profiles/L0rdLost/certificates/6c152bd262967f8c941c9707ed636bda" target="_blank" rel="noopener noreferrer"><li>Python</li></a>
-                    <a href="https://coursera.org/share/cd285a6c835268abec1d6ab014e1b799" target="_blank" rel="noopener noreferrer"><li>JavaScript</li></a>
-                    <a href="https://coursera.org/share/53cbe5a0c6f0697e37b3ee7ed2c8f555" target="_blank" rel="noopener noreferrer"><li>Version Control</li></a>
-                    <a href='https://www.codecademy.com/profiles/L0rdLost/certificates/43dd707594a64f9e8f93b727e16eb9dd' target="_blank" rel="noopener noreferrer"><li>React</li></a>
-                    <a href='https://www.codecademy.com/profiles/L0rdLost/certificates/6635cb9acaac43e3b713d72debc2149b' target='_black' rel='noopener noreferrer'><li>Redux</li></a>
-                    <a href="https://coursera.org/share/86f1339ed69f3dc72701e7d3fd8fab21" target="_blank" rel="noopener noreferrer"><li>HTML & CSS</li></a>
-                </ul>
+                <div className='certifications-card'>
+                    <ul>
+                        <a href="https://www.codecademy.com/profiles/L0rdLost/certificates/6c152bd262967f8c941c9707ed636bda" target="_blank" rel="noopener noreferrer"><li>Python</li></a>
+                        <a href="https://coursera.org/share/cd285a6c835268abec1d6ab014e1b799" target="_blank" rel="noopener noreferrer"><li>JavaScript</li></a>
+                        <a href="https://coursera.org/share/53cbe5a0c6f0697e37b3ee7ed2c8f555" target="_blank" rel="noopener noreferrer"><li>Version Control</li></a>
+                        <a href='https://www.codecademy.com/profiles/L0rdLost/certificates/43dd707594a64f9e8f93b727e16eb9dd' target="_blank" rel="noopener noreferrer"><li>React</li></a>
+                        <a href='https://www.codecademy.com/profiles/L0rdLost/certificates/6635cb9acaac43e3b713d72debc2149b' target='_black' rel='noopener noreferrer'><li>Redux</li></a>
+                        <a href="https://coursera.org/share/86f1339ed69f3dc72701e7d3fd8fab21" target="_blank" rel="noopener noreferrer"><li>HTML & CSS</li></a>
+                    </ul>
+                </div>
             </div>
             <div className='details'>
                 <h1>Details</h1>
-                <h4>Education:</h4>
-                <p>Cal Poly Pomona</p>
-                <p>Bachelor's of Science Applied Mathematics</p>
-                <h4>Location:</h4>
-                <p>California, US</p>
+                <div className='details-content'>
+                    <div className='detail-item'>
+                        <h4>Education:</h4>
+                        <p>California State Polytechnic University, Pomona</p>
+                        <p>Bachelor's of Science Applied Mathematics</p>
+                    </div>
+                    <div className='detail-item'>
+                        <h4>Location:</h4>
+                        <p>California, US</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
