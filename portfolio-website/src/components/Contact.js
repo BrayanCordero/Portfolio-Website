ce9966b9ef596b804766a1480f9c2f74aa62cb0d
BrayanCordero <corderobrayan@yahoo.com>
2024-08-20 18:46:07 -0700

    added styling to contact page, created a form for contact

--- portfolio-website/src/components/Contact.js
+++ portfolio-website/src/components/Contact.js
@@ -1,12 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../styles/ContactStyle.css'
 
 const Contact = () => {
+
+    const [name, setName] = useState('');
+    const [company, setCompany] = useState('');
+    const [email, setEmail] = useState('')
+
+    const Submit = (event) => {
+        event.preventDefault();
+        //handle submit
+
+        setName('');
+        setCompany('');
+        setEmail('');
+    };
+
     return (
-        <div>
-            <h1>Contact Me</h1>
-            <p>corderobrayan@yahoo.com</p>
-            <a href='https://www.linkedin.com/in/brayan-cordero-alvarez-a0a290206' target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        <div className='contact-container'>
+            <div className='form-container'>
+                <form onSubmit={Submit}>
+                    <div className='form'>
+                        <label htmlFor='name'>Name:</label>
+                        <input type='text' id='name' name='name' value={name} onChange={(e) => setName(e.target.value)} required />
+                    </div>
+
+                    <div className='form'>
+                        <label htmlFor='company'>Company Name:</label>
+                        <input type='text' id='company' name='company' value={company} onChange={(e) => setCompany(e.target.value)} required />
+                    </div>
+
+                    <div className='form'>
+                    <label htmlFor='email'>Email:</label>
+                    <input type='email' id='email' name='email' value={email} onChange={(e) => setEmail(e.target.value)} required/>
+                    </div>
+
+                    <div className='form'>
+                    <button type='submit' className='submit-btn'>Submit</button>
+                    </div>
+                </form>
+                {/* <a href='https://www.linkedin.com/in/brayan-cordero-alvarez-a0a290206' target="_blank" rel="noopener noreferrer">LinkedIn</a> */}
+            </div>
         </div>
     )
 };
